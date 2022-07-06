a0c9d82932620c5330faf79e3ecd07aa7ac50f58
Ednardo Gomes <ednardo@email.com>
2022-07-05 22:09:56 -0300

    Configurando o layout da Home.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import { Home } from "./pages/Home";
 
 import { GlobalStyled } from "./global";
 
 export function App() {
   return (
-    <>
-      <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
       <GlobalStyled />
-    </>
+    </BrowserRouter>
   );
 }
