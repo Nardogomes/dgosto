72c497be29449c059a8619691e26d03cb3507b86
Ednardo Gomes <ednardo@gomes.com>
2022-07-12 14:51:29 -0300

    Criando a página Signup.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { Signin } from "./pages/Signin";
+import { Signup } from "./pages/Signup";
 import { Home } from "./pages/Home";
 import { Bolos } from "./pages/Bolos";
 
@@ -11,6 +12,7 @@ export function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/home" element={<Home />} />
         <Route path="/bolos" element={<Bolos />} />
       </Routes>
