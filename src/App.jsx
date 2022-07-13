91ff9f5d2e2852c230bb8ec0ba8e5e1eb6d88859
Ednardo Gomes <ednardo@email.com>
2022-07-12 21:40:23 -0300

    Criando as páginas Doces e Mousses.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { Signin } from "./pages/Signin";
 import { Signup } from "./pages/Signup";
 import { Home } from "./pages/Home";
 import { Bolos } from "./pages/Bolos";
+import { Doces } from "./pages/Doces";
+import { Mousses } from "./pages/Mousses";
 
 import { GlobalStyled } from "./global";
 
@@ -15,6 +17,8 @@ export function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/home" element={<Home />} />
         <Route path="/bolos" element={<Bolos />} />
+        <Route path="/doces" element={<Doces />} />
+        <Route path="/mousses" element={<Mousses />} />
       </Routes>
       <GlobalStyled />
     </BrowserRouter>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -21,20 +21,26 @@ export function Home() {
               <h2>Bolos</h2>
             </div>
           </Link>
-          <div className="card">
-            <img
-              src="https://images.unsplash.com/photo-1630953899906-d16511a72558?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80"
-              alt="Doces"
-            />
-            <h2>Doces</h2>
-          </div>
-          <div className="card">
-            <img
-              src="https://images.unsplash.com/photo-1548329408-0bcd6e68058d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
-              alt="Mousses"
-            />
-            <h2>Mousses</h2>
-          </div>
+
+          <Link to={"/doces"}>
+            <div className="card">
+              <img
+                src="https://images.unsplash.com/photo-1630953899906-d16511a72558?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80"
+                alt="Doces"
+              />
+              <h2>Doces</h2>
+            </div>
+          </Link>
+
+          <Link to={"/mousses"}>
+            <div className="card">
+              <img
+                src="https://images.unsplash.com/photo-1548329408-0bcd6e68058d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+                alt="Mousses"
+              />
+              <h2>Mousses</h2>
+            </div>
+          </Link>
         </div>
       </Content>
     </Container>
